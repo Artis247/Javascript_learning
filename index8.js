fca4ad0d9ba9f6f5e60220689b66ba0de1f69464
Artur <75115204+Artis247@users.noreply.github.com>
2022-10-12 09:52:27 +0300

    second

---
+++ index8.js
@@ -0,0 +1,89 @@
+//Javascript_proxy
+
+
+
+//1.Objects
+const person = {
+    name:'Denis',
+    age:25,
+    job:'Fullstack'
+}
+const op = new Proxy(person,{
+    get(target,prop){
+        console.log('target',target)
+        console.log('prop', prop)
+
+        return target[prop]
+    },
+    set(target,prop,value){
+        if(prop in target){
+       target[prop] = value 
+        }else{
+            throw new Error('No prop field in target')
+        }
+    },
+    has(target, prop){
+        return ['age','name','job'].includes(prop)
+    },
+    deleteProperty(target, prop){
+        console.log('Deleting...', prop)
+        delete target[prop]
+        return true
+    }
+
+    
+})
+
+// Functions
+
+const log = text => 'log :'+ text
+
+const fp = new Proxy(log,{  //объект хэндлеров
+    apply(target,thisArg,args){
+     console.log('Calling fn ..')   
+
+     return target.apply(thisArg, args).toUpperCase()
+    }
+})
+
+//Classes
+
+class Person {
+    constructor(name,age){
+        this.name = name
+        this.age = age
+    }
+}
+const PersonProxy = new Proxy (Person,{
+    construct(target,args){
+        console.log("Construct ..")
+
+        return new Proxy(new target(...args),{
+            get(t,prop){
+                console.log("Getting prop ..",prop)
+                return t[prop]
+            }
+        })
+    }
+})
+const p = new PersonProxy('Maxim',30)
+
+//ПРИМЕР
+
+const exampleProxyObjedct =new Proxy(person,{
+    get(target,prop){
+        console.log('target',target)
+        console.log('prop', prop)
+
+        if(!(prop in target)){
+            return prop
+            .split('_')
+            .map(p => target[p])
+            .join(' ')
+        }
+
+        return target[prop]
+    }
+
+    
+})
---
+++ index9.js
@@ -0,0 +1,1 @@
+//Javascript_proxy_USING_EXAMPLES
